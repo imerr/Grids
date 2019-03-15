3805a36a59295f64ee31268f7ae9a590d8a9a0f4
Robin Rolf <imer@imer.cc>
2019-03-15 04:53:33 +0100

    Laravel 5.8 compatibility

--- src/Components/Pager.php
+++ src/Components/Pager.php
@@ -1,85 +1,61 @@
 <?php
 namespace Nayjest\Grids\Components;
 
-use DomainException;
-use Illuminate\Foundation\Application;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Input;
 use Nayjest\Grids\Components\Base\RenderableComponent;
 use Nayjest\Grids\Grid;
 
 class Pager extends RenderableComponent
 {
-
-    /**
-     * @var \Illuminate\Pagination\Factory
-     */
-    protected $pagination_factory;
-
     protected $input_key;
 
     protected $previous_page_name;
 
-    public function __construct()
-    {
-        if (version_compare(Application::VERSION, '5', '>')) {
-            $className = get_class($this);
-            throw new DomainException(
-                "$className designed for usage only with Laravel 4.X"
-            );
-        }
-    }
+    protected $name = 'pager';
 
-    /**
-     * {@inheritdoc}
-     */
     public function render()
     {
+        $this->setupPaginationForLinks();
         $result = (string)$this->links();
         return $result;
     }
 
     protected function setupPaginationForReading()
     {
-        $this->pagination_factory->setPageName("$this->input_key.page");
+        Paginator::currentPageResolver(function () {
+            return Input::get("$this->input_key.page", 1);
+        });
     }
 
     protected function setupPaginationForLinks()
     {
-        $this->pagination_factory->setPageName("{$this->input_key}[page]");
-    }
-
-    protected function restorePaginationOptions()
-    {
-        $this->pagination_factory->setPageName($this->previous_page_name);
-    }
-
-    protected function links()
-    {
-
-        $this->setupPaginationForReading();
         /** @var  Paginator $paginator */
         $paginator = $this->grid->getConfig()
             ->getDataProvider()
             ->getPaginator();
+        $paginator->setPageName("{$this->input_key}[page]");
+    }
 
-        $this->setupPaginationForLinks();
+    /**
+     * Renders pagination links & returns rendered html.
+     */
+    protected function links()
+    {
+        /** @var  Paginator $paginator */
+        $paginator = $this->grid->getConfig()
+            ->getDataProvider()
+            ->getPaginator();
         $input = $this->grid->getInputProcessor()->getInput();
         if (isset($input['page'])) {
             unset($input['page']);
         }
-        $res = (string)$paginator->appends($this->input_key, $input)->links();
-        $this->restorePaginationOptions();
-        return $res;
+        return str_replace('/?', '?',$paginator->appends($this->input_key, $input)->render());
     }
 
     public function initialize(Grid $grid)
     {
         parent::initialize($grid);
-        $this->pagination_factory = $grid
-            ->getConfig()
-            ->getDataProvider()
-            ->getPaginationFactory();
-        $this->previous_page_name = $this->pagination_factory->getPageName();
         $this->input_key = $grid->getInputProcessor()->getKey();
         $this->setupPaginationForReading();
     }
